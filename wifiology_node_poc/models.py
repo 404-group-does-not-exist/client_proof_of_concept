a1826f56c55639922048043320b84b989c2ecf58
Jason Nguyen <jasonn@engr2-2-75-10-dhcp.int.colorado.edu>
2019-04-05 18:49:44 -0600

    Integrated the new schema changes

--- wifiology_node_poc/models.py
+++ wifiology_node_poc/models.py
@@ -44,23 +44,20 @@ class RecordObject(object):
 
 class Measurement(RecordObject):
     def __init__(self, measurement_id, measurement_start_time, measurement_end_time, measurement_duration,
-                 channel, management_frame_count, control_frame_count, data_frame_count, extra_data):
+                 channel, frame_counts, extra_data):
         self.measurement_id = measurement_id
         self.measurement_start_time = measurement_start_time
         self.measurement_end_time = measurement_end_time
         self.measurement_duration = measurement_duration
         self.channel = channel
-        self.management_frame_count = management_frame_count
-        self.control_frame_count = control_frame_count
-        self.data_frame_count = data_frame_count
+        self.frame_counts = frame_counts
         self.extra_data = extra_data
 
     def __repr__(self):
         return "Measurement(measurementID={id}, startTime={st}, endTime={et}, duration={d}, channel={c}, " \
-               "managementFrameCount={mfc}, controlFrameCount={cfc}, dataFrameCount={dfc})".format(
+               "frameCounts={fc})".format(
                 id=self.measurement_id, st=self.measurement_start_time, et=self.measurement_end_time,
-                d=self.measurement_duration, c=self.channel, mfc=self.management_frame_count,
-                cfc=self.control_frame_count, dfc=self.data_frame_count
+                d=self.measurement_duration, c=self.channel, fc=repr(self.frame_counts)
                )
 
     @classmethod
@@ -74,52 +71,57 @@ class Measurement(RecordObject):
                 row[prefix + "measurementEndTime"],
                 row[prefix + "measurementDuration"],
                 row[prefix + "channel"],
-                row[prefix + "managementFrameCount"],
-                row[prefix + "controlFrameCount"],
-                row[prefix + "dataFrameCount"],
+                FrameCounts.from_row(row, prefix=prefix),
                 cls._json_loads(row[prefix + "extraJSONData"])
             )
 
     @classmethod
-    def new(cls, start_time, end_time, duration, channel, management_frame_count, control_frame_count,
-            data_frame_count, extra_data=None):
+    def new(cls, start_time, end_time, duration, channel, frame_counts, extra_data=None):
         return cls(
             None,
             start_time,
             end_time,
             duration,
             channel,
-            management_frame_count,
-            control_frame_count,
-            data_frame_count,
+            frame_counts,
             extra_data or {}
         )
 
     def to_row(self, prefix=""):
-        return {
+        base_row = {
             prefix + 'measurementID': self.measurement_id,
             prefix + 'measurementStartTime': self.measurement_start_time,
             prefix + 'measurementEndTime': self.measurement_end_time,
             prefix + 'measurementDuration': self.measurement_duration,
             prefix + 'channel': self.channel,
-            prefix + 'managementFrameCount': self.management_frame_count,
-            prefix + 'controlFrameCount': self.control_frame_count,
-            prefix + 'dataFrameCount': self.data_frame_count,
             prefix + 'extraJSONData': self._json_dumps(self.extra_data)
         }
+        base_row.update(self.frame_counts.to_row(prefix=prefix))
+        return base_row
 
     def to_api_response(self):
-        return {
+        base_response = {
             'measurementID': self.measurement_id,
             'measurementStartTime': self.measurement_start_time,
             'measurementEndTime': self.measurement_end_time,
             'measurementDuration': self.measurement_duration,
             'channel': self.channel,
-            'managementFrameCount': self.management_frame_count,
-            'controlFrameCount': self.control_frame_count,
-            'dataFrameCount': self.data_frame_count,
             'extraData': self.extra_data
         }
+        base_response.update(self.frame_counts.to_api_response())
+        return base_response
+
+    @property
+    def data_frame_count(self):
+        return self.frame_counts.data_frame_count
+
+    @property
+    def control_frame_count(self):
+        return self.frame_counts.control_frame_count
+
+    @property
+    def management_frame_count(self):
+        return self.frame_counts.management_frame_count
 
 
 class Station(RecordObject):
@@ -202,4 +204,58 @@ class ServiceSet(RecordObject):
             'networkName': self.network_name,
             'extraData': self.extra_data
         }
+
+class FrameCounts(RecordObject):
+    def __init__(self, management_frame_count, control_frame_count, 
+                 rts_frame_count, cts_frame_count, ack_frame_count, 
+                 data_frame_count, data_throughput):
+        self.management_frame_count = management_frame_count
+        self.control_frame_count = control_frame_count
+        self.rts_frame_count = rts_frame_count
+        self.cts_frame_count = cts_frame_count
+        self.ack_frame_count = ack_frame_count
+        self.data_frame_count = data_frame_count
+        self.data_throughput = data_throughput
+    
+    @classmethod
+    def from_row(cls, row, prefix=""):
+        if row is None:
+            return None
+        else:
+            return cls(
+                row[prefix + "managementFrameCount"],
+                row[prefix + "controlFrameCount"],
+                row[prefix + "rtsFrameCount"],
+                row[prefix + "ctsFrameCount"],
+                row[prefix + "ackFrameCount"],
+                row[prefix + "dataFrameCount"],
+                row[prefix + "dataThroughput"],
+            )
+
+    def to_row(self, prefix=""):
+        return {
+            prefix + 'managementFrameCount' : self.management_frame_count,
+            prefix + 'controlFrameCount' : self.control_frame_count,
+            prefix + 'rtsFrameCount' : self.rts_frame_count,
+            prefix + 'ctsFrameCount' : self.cts_frame_count,
+            prefix + 'ackFrameCount' : self.ack_frame_count,
+            prefix + 'dataFrameCount' : self.data_frame_count,
+            prefix + 'dataThroughput' : self.data_throughput,
+        }
+
+    def to_api_response(self):
+        return {
+            'managementFrameCount' : self.management_frame_count,
+            'controlFrameCount' : self.control_frame_count,
+            'rtsFrameCount' : self.rts_frame_count,
+            'ctsFrameCount' : self.cts_frame_count,
+            'ackFrameCount' : self.ack_frame_count,
+            'dataFrameCount' : self.data_frame_count,
+            'dataThroughput' : self.data_throughput,
+        }
+
+
+
+
+
 
